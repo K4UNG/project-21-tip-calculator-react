504c10a84914a4e54517d0d466249374defd1366
K4UNG <kgzinhein.my@gmail.com>
2022-04-25 09:59:28 +0630

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import TipInput from './components/TipInput/TipInput'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>spli<br/>tter</h1>
+
+      <div className='wrapper'>
+        <TipInput />
+      </div>
+    </div>
+  );
+}
+
+export default App;
